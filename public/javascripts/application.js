f4c27341b84c44c5ee0ba77aff88e00224d587b1
Justin B. Kay <justinbkay@gmail.com>
2011-06-26 15:06:59 -0600

    adding a confirmation in the js delete

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,3 +4,13 @@
 $(function (){  
     $('#event_date').datepicker({ dateFormat: 'yy-mm-dd' });
 });
+
+$('.delete').click(function(model_name, item_id){
+  var answer = confirm('Are you sure you want to delete this?');
+  if (answer) {
+		$('#' + model_name + '_' + item_id).hide();
+		$.post('/' + model_name + '/' + item_id, {_method : 'delete', authenticity_token : '<%= form_authenticity_token %>'});
+	} else {
+		return false
+	}
+});
