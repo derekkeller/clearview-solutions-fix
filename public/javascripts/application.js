649d832f3429ad09f9c3a442fe005f90cabaaac6
Justin B. Kay <justinbkay@gmail.com>
2011-06-26 21:40:03 -0600

    doing the delete with unobtrusive javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,14 +3,14 @@
 
 $(function (){  
     $('#event_date').datepicker({ dateFormat: 'yy-mm-dd' });
-});
 
-$('.delete').click(function(model_name, item_id){
-  var answer = confirm('Are you sure you want to delete this?');
-  if (answer) {
-		$('#' + model_name + '_' + item_id).hide();
-		$.post('/' + model_name + '/' + item_id, {_method : 'delete', authenticity_token : '<%= form_authenticity_token %>'});
-	} else {
-		return false
-	}
+		$('.destroy').click(function () {
+			var answer = confirm('Are you sure you want to delete this?');
+		  if (answer) {
+				$('#' + this.dataset['model'] + '_' + this.dataset['id']).hide();
+				$.post('/' + this.dataset['model'] + '/' + this.dataset['id'], {_method : 'delete', authenticity_token : $('meta[name=csrf-token]').attr("content")});
+			} else {
+				return false
+			}
+		});
 });
